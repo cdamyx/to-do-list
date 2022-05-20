a6dddf63bc392fe39778349995777d434d607d95
cdamyx <chris@amyx.engineering>
2022-05-19 22:07:43 -0500

    final cleanup and ADA checks

--- app.js
+++ app.js
@@ -11,102 +11,98 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 mongoose.connect(uri, {
-
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
 });
 
 const itemsSchema = {
-  name: String
+    name: String
 };
 
 const Item = mongoose.model("Item", itemsSchema);
 
 let completedList = [];
 
-// {id: 1, name: "one"},
-//   {id: 2, name: "two"},
-//   {id: 3, name: "three"}
-
-
 app.get("/", (req, res) => {
 
-  Item.find({}, (err, docs) => {
+    Item.find({}, (err, docs) => {
 
-    if (!err) {
-      res.render("list", {listTitle: "To-Do List", newListItems: docs});
-    } else {
-      console.log(err);
-    }
-  });
+        if (!err) {
+            res.render("list", { listTitle: "To-Do List", newListItems: docs });
+        } else {
+            console.log(err);
+        }
+    });
 });
 
 app.post("/", async (req, res) => {
 
-  const itemName = req.body.newItem;
+    const itemName = req.body.newItem;
 
-  const item = new Item ({
-    name: itemName
-  });
+    const item = new Item({
+        name: itemName
+    });
 
-  await item.save();
-  res.redirect("/");
+    await item.save();
+    res.redirect("/");
 });
 
 app.post("/delete", (req, res) => {
-  
-  const deleteItemId = req.body.delete;
 
-  Item.findByIdAndRemove(deleteItemId, (err) => {
-    if(!err) {
-      console.log("deleted");
-      res.redirect("/");
-    }
-  });
+    const deleteItemId = req.body.delete;
+
+    Item.findByIdAndRemove(deleteItemId, (err) => {
+        if (!err) {
+            console.log("deleted");
+            res.redirect("/");
+        } else {
+            console.log(err);
+        }
+    });
 });
 
-app.get("/about", function(req, res){
-  res.render("about");
-});
-
-app.get("/completed", function(req, res){
-  res.render("completed", {listTitle: "Completed", newListItems: completedList});
+app.get("/completed", function(req, res) {
+    res.render("completed", { listTitle: "Completed", newListItems: completedList });
 });
 
 app.post("/completed", (req, res) => {
-  const checkedItemId = req.body.checkbox;
+    const checkedItemId = req.body.checkbox;
 
-  Item.findOne({_id : checkedItemId}, async (err, docs) => {
+    Item.findOne({ _id: checkedItemId }, async (err, docs) => {
 
-    if(!err) {
-      completedList.length > 24? completedList.unshift(docs.name) && completedList.pop() : completedList.unshift(docs.name);
-      
-      await Item.findByIdAndRemove(checkedItemId, (err) => {
-        if(!err) {
-          console.log("Completed item removed from DB");
+        if (!err) {
+            completedList.length > 24 ? completedList.unshift(docs.name) && completedList.pop() : completedList.unshift(docs.name);
+
+            await Item.findByIdAndRemove(checkedItemId, (err) => {
+                if (!err) {
+                    console.log("Completed item removed from DB");
+                } else {
+                    console.log("Error deleting completed item from DB: ", err);
+                }
+            });
+
+            res.redirect("/");
+
         } else {
-          console.log("Error deleting completed item from DB: ", err);
+            console.log("Error finding item in DB: ", err);
         }
-      });
+    })
+});
 
-      res.redirect("/");
-
-    } else {
-      console.log("Error finding item in DB: ", err);
-    }
-  })
+app.get("/about", function(req, res) {
+    res.render("about");
 });
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 3000;
+    port = 3000;
 }
 
 app.listen(port, function() {
-  console.log("Server has started successfully.");
+    console.log("Server has started successfully.");
 });
